d6b0f86fb6fe524747ed4f4555c72ab79f39ce72
Pavlo Dragobetskij <pal-mcc@gmx.com>
2016-09-25 17:27:11 +0300

    start

---
+++ GigHub/Models/Genre.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GigHub.Models
+{
+    public class Genre
+    {
+        [Required]
+        public Byte Id { get; set; }
+        [Required]
+        public string GenreName { get; set; }
+    }
+}
---
+++ GigHub/Models/Gig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GigHub.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+        [Required]
+        public ApplicationUser Author { get; set; }
+        public DateTime Date { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Venue { get; set; }
+        public Genre Genre { get; set; }
+
+    }
+}
